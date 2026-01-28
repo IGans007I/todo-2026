c0ce7a5b1c486bfea838c3dc6df6d865beb55289
poma098 <1@insoweb.ru>
2026-01-28 19:27:32 +0500

    feat: 4xx and 5xx

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,34 @@ function buildApp() {
 
   app.use('/health', healthRouter);
 
+  app.use((req, res) => {
+    res.status(404).json({
+      status: "Not found",
+    });
+  });
+
+  app.use((err: unknown, _req: express.Request, res: express.Response) => {
+
+    console.error(err);
+    
+    if (err instanceof Error) {
+      return res.status(500).json({
+        status: 'Internal Server Error',
+        message: err.message,
+      });
+    } else if (typeof err === 'string') {
+      return res.status(500).json({
+        status: 'Internal Server Error',
+        message: err,
+      });
+    } else {
+      res.status(500).json({
+        status: 'Error',
+        message: 'Internal Server Error',
+      });
+    }
+  });
+
   return app;
 }
 
